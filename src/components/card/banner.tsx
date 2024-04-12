9cf390d41db0273f7af239372d5781b710b9379c
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-04-12 12:06:10 +0300

    fix:removed data

--- src/components/card/banner.tsx
+++ src/components/card/banner.tsx
@@ -12,12 +12,7 @@ const Banner = ({
   title: string;
   desc: string;
 }) => {
-  const data = {
-    number: 1,
-    topic: "CODE NIGHT COMMUNITY",
-    title: "Community of 🇪🇹 developers to showcase their projects",
-    desc: "We're an international group of Bug Hunters keeping Google products and the Internet safe and secure.",
-  };
+
   return (
     <div className="flex flex-col gap-6 p-6">
       <Header number={number} topic={topic} />
